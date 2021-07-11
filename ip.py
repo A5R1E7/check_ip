90d505a5d860eddd32ee50bcdcec79d358d4f5b9
A5R1E7 <87268977+A5R1E7@users.noreply.github.com>
2021-07-11 16:12:01 +0200

    Add files via upload

---
+++ ip.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+import socket
+app = Flask(__name__)
+IPList=[]
+@app.route('/ip')
+def ip():
+    h_name = socket.gethostname()
+    IP_address = socket.gethostbyname(h_name)
+    IPList.append(IP_address)
+    return "Twój adres IP:  " + IP_address
+
+@app.route('/ip/list')
+def test_json():
+    return jsonify(Lista= IPList)
+
+@app.route('/')
+def hello():
+     return app.send_static_file('index.html')
+if __name__=='__main__':
+    app.run(debug=True, port=8000)
