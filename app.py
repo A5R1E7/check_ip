d2388c42abfd17c41f9d8985dd29d3c67744609f
Shiarise <87043193+Shiarise@users.noreply.github.com>
2021-07-13 12:23:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,36 @@
 from flask import Flask, render_template, request, send_file,redirect,url_for, Response, redirect, jsonify
-
-from flask_accept import accept
+from  flask_accept  import  accept 
 
 app = Flask(__name__,static_folder="static", template_folder="templates")
 import sys
 import os
 import socket
 
-payload = {}
-headers = {header: accept (yaml, json, text, html)}
-print(response.text.encode('utf8'))
-response = requests.get(localhost, headers=headers)
 IPList=[]
+
+@app.route('/')
+@accept('text/html')
+def hello():
+    return "Strona testowa"
 @app.route('/ip')
+
 def ip():
     h_name = socket.gethostname()
     IP_address = socket.gethostbyname(h_name)
     IPList.append(IP_address)
     return "Twój adres IP:  " + IP_address
 
+@test_json.support('application/json')
+@test_json.support('application/text')
+@test_json.support('application/html')
+@test_json.support('application/yaml')
 @app.route('/ip/list')
 def test_json():
-    return jsonify(Lista= IPList)
+    return jsonify(Lista = IPList)
 
-@app.route('/')
-@accept ('text/html') 
-def hello():
-    return "Strona testowa"
-header: accept (yaml, json, text, html)
-r = requests.get('https://github.com/A5R1E7/check_ip.git', auth=('A5R1E7', 'Divingvatt1'))
-r.status_code
-r.headers['content-type']
-r.encoding
-r.text
-r.json()
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=8080)
